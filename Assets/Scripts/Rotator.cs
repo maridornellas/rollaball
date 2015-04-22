c80667789f1fc4248cc4421f23f007ba1c0f3fb4
Mariana D' Ornellas <mdornellas@stu.aii.edu>
2015-04-21 20:13:02 -0700

    [RollABall]  - Initial Commit.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float xSpeed;
+	public float ySpeed;
+	public float zSpeed;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 rotation = new Vector3 (xSpeed, ySpeed, zSpeed);
+		transform.Rotate(rotation);
+	}
+}
---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreTracker : MonoBehaviour
+{
+	static public ScoreTracker Instance;
+
+	Text scoreText;
+	int score = 0;
+
+	void Awake()
+	{
+		Instance = this;
+
+		scoreText = GetComponent<Text> ();
+		scoreText.text = "Score: " + score.ToString ();
+	}
+
+	public void AddScore(int value)
+	{
+		score += value;
+		scoreText.text = "Score: " + score.ToString ();
+	}
+}
