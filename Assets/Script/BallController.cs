57cc94135364e8a1fab4d108de8451990e005566
Mariana D' Ornellas <mdornellas@stu.aii.edu>
2015-04-21 21:03:20 -0700

    [RollABall]  - Correct Version

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public float moveSpeed;
+	public float jumpSpeed;
+
+
+	bool isGrounded = true;
+
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+				float x = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+				float z = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+				Vector3 force = new Vector3 (x, 0f, z);
+				if (isGrounded && Input.GetButton ("Jump")) {
+						force.y = jumpSpeed;
+						isGrounded = false;
+				}
+				rigidbody.AddForce (force);
+		}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.CompareTag("Ground"))
+			{
+				isGrounded = true;
+			}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Coin"))
+		other.gameObject.SetActive(false);
+		ScoreTracker.Instance.AddScore (10);
+	}
+}
---
+++ Assets/Script/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿//libraries
+using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject target; //view target object
+	Vector3 offset; //position relative to our view target
+
+	void Start () 
+	{
+		//compute the offset relative to our target
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = target.transform.position + offset;
+	}
+}
